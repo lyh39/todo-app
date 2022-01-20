b60f590b20d08dfdd18b63e66e26a7138af174d1
nawal.bhengra@stackmybiz.com <nawal.bhengra@stackmybiz.com>
2022-01-20 17:02:08 +0530

    arrangements of designs and alignments

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import { TodoScreen } from '../src/components/TodoScreen'
 function App() {
   return (
-    <div className="flex justify-center min-h-screen bg-blue-200">
-      <div >
-        <div className="flex justify-center bg-red-300">
-          <text className="text-xl font-bold">
+    <div className="flex justify-center min-h-screen">
+      <div className="mt-12">
+        <div className="flex justify-center border-b border-slate-300 pb-1">
+          <text className="text-xl">
             THINGS TO DO:
           </text>
         </div>
         <div >
-         <TodoScreen />
+          <TodoScreen />
         </div>
       </div>
     </div>
--- src/components/TodoScreen.js
+++ src/components/TodoScreen.js
@@ -1,73 +1,73 @@
 import React, { useState } from 'react';
-import { Formik, Form, Field } from 'formik'
-import { WorkToDo } from '../FormShema/WorkToDo'
-import CancelPresentationIcon from '@mui/icons-material/CancelPresentation';
+import { Formik, Form, Field } from 'formik';
+import { WorkToDo } from '../FormShema/WorkToDo';
+import CloseIcon from '@mui/icons-material/Close';
 
 export const TodoScreen = () => {
     const [workList, setWorkList] = useState([]);
     const [countWorkDone, setContWorkDone] = useState([]);
 
-    // const WorkToDo = Yup.object().shape({
-    //     todoName: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('required')
-    // });
-
     const todoDone = (event) => {
-        let id = event.target.value
+        let id = event.target.value;
+        let arr = [];
         workList.forEach((e) => {
             if (e.id === Number(id)) {
                 e.status = !e.status;
-            }
-        })
-        setWorkList(workList)
-        let arr = [];
+            };
+        });
+        setWorkList(workList);
         workList.forEach((eventVal) => {
             if (eventVal.status === true) {
-                arr.push({ id: eventVal.id, status: eventVal.status })
-            }
-        })
-        setContWorkDone(arr)
-    }
+                arr.push({ id: eventVal.id, status: eventVal.status });
+            };
+        });
+        setContWorkDone(arr);
+    };
 
     const removeTodo = (event) => {
         let arr = [];
         let arrCount = [];
         workList.filter((idVal) => {
             if (event !== idVal.id) {
-                arr.push(idVal)
-            }
-        })
+                arr.push(idVal);
+            };
+        });
         setWorkList(arr);
         countWorkDone.filter((ival) => {
-            console.log(ival)
             if (event !== ival.id) {
-                arrCount.push(ival)
-            }
-        })
-        setContWorkDone(arrCount)
-    }
+                arrCount.push(ival);
+            };
+        });
+        setContWorkDone(arrCount);
+    };
 
 
     return (
-        <div className='bg-green-300 p-10'>
-            {workList.length === 0 ? <div><text>Looks like you are absoluteky free today!</text></div> :
-                <div>{workList.map(e => (
-                    <div className='flex justify-between' key={e.id}>
+        <div className='py-4'>
+            {workList.length === 0 ?
+                <div className='flex justify-center text-sm border-b border-slate-300 w-full pb-4'><text className='text-grey-400'>Looks like you are absoluteky free today!</text></div>
+                :
+                <div className='border-b border-slate-300 pb-4'>{workList.map(e => (
+                    <div className='flex justify-between pt-1 pb-1' key={e.id}>
                         <div>
-                            <input className='mr-2' value={e.id} type="checkbox" onClick={todoDone}
+                            <input className='mr-2 focus:ring-0' style={{height:12, width:12}} value={e.id} type="checkbox" onClick={todoDone}
                             />
                             {e.status ?
-                                <span className='line-through'>{e.value}</span>
+                                <span className='line-through text-sm'>{e.value}</span>
                                 :
-                                <span>{e.value}</span>}
+                                <span className='text-sm'>{e.value}</span>}
                         </div>
                         <div>
                             <button onClick={() => removeTodo(e.id)}>
-                                <CancelPresentationIcon fontSize="small" />
+                                <CloseIcon style={{fontSize: 14}} className='bg-grey-200' />
                             </button>
                         </div>
                     </div>
-                ))}</div>}
-            <div className='flex justify-center'>
+                ))
+                }
+                </div>
+            }
+            <div className='flex justify-center mt-2'>
                 {countWorkDone.length <= 0 ? <text>DONE:</text> : <text>DONE: {countWorkDone.length}</text>}
             </div>
             <Formik
@@ -83,17 +83,16 @@ export const TodoScreen = () => {
             >
                 {(props) => (
                     <Form>
-                        <div className='flex justify-center w-full'>
+                        <div className='flex justify-center w-full mt-2'>
                             <div className='pr-1 '>
                                 <div>
-                                    <Field className="rounded border-none shadow-md" type="text" name="todoName" placeholder="Enter New Task" />
+                                    <Field className="rounded shadow-md text-sm" type="text" name="todoName" placeholder="Enter New Task" />
                                 </div>
                                 {props.errors.todoName ? <div className='pt-2'>
                                     <text className='text-red-500'>{props.errors.todoName}</text>
                                 </div> : null}
-
                             </div>
-                            <div className=''>
+                            <div >
                                 <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow-md">
                                     ADD TASK
                                 </button>
@@ -104,5 +103,4 @@ export const TodoScreen = () => {
             </Formik>
         </div >
     );
-}
-
+};
