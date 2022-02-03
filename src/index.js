5e7502b2bdb0f2c101874179c32e9206f54e5c51
nawal <nawal.bhengra@stackmybiz.com>
2022-02-03 18:43:03 +0530

    redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/styles/main.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { mainReducer } from './reducer'
+
+const store = createStore(mainReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// store.subscribe(()=> console.log('===>Store value',store.getState()));
+// console.log('initialstate', store.getState().counterReducer.counter);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
